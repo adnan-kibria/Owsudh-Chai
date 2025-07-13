1d60c7b2824e4ccba5bbb16b02812c9a443d183d
Golam Kibria <82614395+adnan-kibria@users.noreply.github.com>
2025-07-13 22:26:52 +0600

    Add files via upload

---
+++ Start.java
@@ -0,0 +1,10 @@
+import Openning.*;
+import Pages.*;
+public class Start 
+{
+    public static void main(String [] args)
+    {
+        OpenningPage enp = new OpenningPage();
+        enp.setVisible(true);
+    }
+}
